b620353fbc0337e38002d5fafa02af83ed24437e
Federico Peña <federico@myabakus.com>
2022-09-04 17:48:51 +1200

    add css variables using style dictionary

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,7 @@
 <template>
-  <h1 class="text-3xl font-bold underline p-6">
-    Hello world!
-  </h1>
+  <div style="background-color:var(--sd-global-colors-black)" class="w-full h-screen">
+    <h1 style="color: var(--sd-global-colors-white);" class="text-3xl font-bold underline p-6">
+      Hello world!
+    </h1>
+</div>
 </template>
--- src/main.js
+++ src/main.js
@@ -1,5 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import './index.css'
+import '/build/css/_variables.css'
 
 createApp(App).mount('#app')
